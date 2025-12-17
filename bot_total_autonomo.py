d27f4626ef36b8637a628f278fb0c5078a203dbc
AnonyMobile <66450414+AnonyMobile@users.noreply.github.com>
2025-12-17 01:31:50 -0300

    Fix syntax error in image resizing method

--- bot_total_autonomo.py
+++ bot_total_autonomo.py
@@ -106,7 +106,7 @@ def criar_ebook(nicho):
 def texto_para_video(texto, output):
     communicate = edge_tts.Communicate(texto, "pt-BR-AntonioNeural")
     asyncio.run(communicate.save("audio.mp3"))
-    img = mp.ImageClip("bg.jpg", duration=60).resize(height=1920).resize(width=1080, method="lanczos", resample="lanczos")
+    img = mp.ImageClip("bg.jpg", duration=60).resize(height=1920").resize(width=1080", method="lanczos", resample="lanczos")
     aud = mp.AudioFileClip("audio.mp3")
     final = img.set_audio(aud)
     final.write_videofile(output, fps=24, codec="libx264", audio_codec="aac", logger=None)
